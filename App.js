2d6514c58f2d72886693bdc67ddf93fd5e87bdd9
Benjamin Chong <chong_meng_fai@s2024.ssts.edu.sg>
2025-05-26 16:12:12 +0800

    hi im not ars

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is lidewally a test</Text>
+      <Text>This is hi im meng fai  a test</Text>
       <StatusBar style="auto" />
     </View>
   );
